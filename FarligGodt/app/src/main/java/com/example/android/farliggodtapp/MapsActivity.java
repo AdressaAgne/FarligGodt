5845383734b32426a2a371bb4a62386f3e606ac7
miatollaksvik <design@miatollaksvik.no>
2016-10-04 15:11:26 +0200

    added settings

--- FarligGodt/app/src/main/java/com/example/android/farliggodtapp/MapsActivity.java
+++ FarligGodt/app/src/main/java/com/example/android/farliggodtapp/MapsActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.farliggodtapp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
@@ -10,6 +11,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -134,4 +136,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+
+    // open next activity //
+    public void openSettings(View view){
+        Intent i = new Intent(this, Settings.class);
+        startActivity(i);
+    }
+
 }
--- FarligGodt/app/src/main/java/com/example/android/farliggodtapp/Settings.java
+++ FarligGodt/app/src/main/java/com/example/android/farliggodtapp/Settings.java
@@ -2,14 +2,46 @@ package com.example.android.farliggodtapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class Settings extends AppCompatActivity {
 
-
+    TextView textViewRadius;
+    SeekBar seekBarRadius;
+    int progress = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        //////////////// SEEKBAR FOR RADIUS ///////////////////////
+
+        seekBarRadius = (SeekBar) findViewById(R.id.radius_bar);
+        seekBarRadius.setMax(100);
+        seekBarRadius.setProgress(progress);
+
+        textViewRadius = (TextView) findViewById(R.id.radiusNumber);
+        textViewRadius.setText(progress + " km");
+
+        seekBarRadius.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                progress = i;
+                textViewRadius.setText(progress + "km");
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        ////////////////////////////////////////////////////////////////////////////////////
     }
 }
