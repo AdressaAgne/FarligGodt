71467458441e06a8e5cf30f7eea11ac3fe728fde
Agne <agne240@me.com>
2016-10-06 19:25:59 +0200

    Radio Buttons working

--- FarligGodt/app/src/main/java/com/example/android/farliggodtapp/Settings.java
+++ FarligGodt/app/src/main/java/com/example/android/farliggodtapp/Settings.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -39,6 +42,7 @@ public class Settings extends AppCompatActivity {
 
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                // 1km in miles 0.621371192, 1mile in km 1.609344
                 progress = i;
                 textViewRadius.setText(progress + "km");
                 db.updateOrInsert("radius", Integer.toString(i));
@@ -53,7 +57,53 @@ public class Settings extends AppCompatActivity {
             }
         });
 
+
+        // Radio Buttons
+
+        RadioGroup distanceType = (RadioGroup) findViewById(R.id.distanceType);
+        Button km = (RadioButton) findViewById(R.id.km);
+        Button miles = (RadioButton) findViewById(R.id.miles);
+        Button nautical = (RadioButton) findViewById(R.id.nautical);
+
+        switch (db.fetchType("distanceType")){
+
+            case "km":
+                distanceType.check(R.id.km);
+                break;
+            case "miles":
+                distanceType.check(R.id.miles);
+                break;
+            case "nautical":
+                distanceType.check(R.id.nautical);
+                break;
+            default:
+                db.updateOrInsert("distanceType", "km");
+                break;
+        }
+
+
+        km.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                db.updateOrInsert("distanceType", "km");
+            }
+        });
+
+        miles.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                db.updateOrInsert("distanceType", "miles");
+            }
+        });
+
+        nautical.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                db.updateOrInsert("distanceType", "nautical");
+            }
+        });
     }
+
 
     // open next activity //
     public void openMain(View view){
