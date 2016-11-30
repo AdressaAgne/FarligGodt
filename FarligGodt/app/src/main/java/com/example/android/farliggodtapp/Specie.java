fc6acca8903a557a3b161f837a45c4c3363b5fca
thomhess <hessthom@gmail.com>
2016-11-30 11:03:29 +0100

    Specie activity + search field

---
+++ FarligGodt/app/src/main/java/com/example/android/farliggodtapp/Specie.java
@@ -0,0 +1,16 @@
+package com.example.android.farliggodtapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by thomhess on 30.11.2016.
+ */
+
+public class Specie extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_specie);
+    }
+}
+
