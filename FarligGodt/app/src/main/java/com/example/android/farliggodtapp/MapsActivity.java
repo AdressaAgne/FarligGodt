4dfc5411fb761a35851aba3bda181a008bfd6600
miatollaksvik <design@miatollaksvik.no>
2016-12-06 15:13:24 +0100

    autocompletttt trash

--- FarligGodt/app/src/main/java/com/example/android/farliggodtapp/MapsActivity.java
+++ FarligGodt/app/src/main/java/com/example/android/farliggodtapp/MapsActivity.java
@@ -16,6 +16,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.SearchView;
 import android.widget.Toast;
 
@@ -55,7 +57,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     private SearchView searchInput;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -79,10 +80,27 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
 
         //Searchfield listener
+
+        AutoCompleteTextView text;
+        //MultiAutoCompleteTextView text1;
+        String[] skjiit={"Mia ","Thomas","Agne","Øyvind","ølksølks","Skjiitsekk"};
+
+
+
+        text=(AutoCompleteTextView)findViewById(R.id.autoCompleteTextView1);
+        //text1=(MultiAutoCompleteTextView)findViewById(R.id.multiAutoCompleteTextView1);
+
+        ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,skjiit);
+
+        text.setAdapter(adapter);
+        text.setThreshold(1);
+
+        //text1.setAdapter(adapter);
+        //text1.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
+        /*
         searchInput = (SearchView)findViewById(R.id.search_input);
-        // Register the onClick listener with the implementation above
-        //searchInput.setOnClickListener(SearchFieldListener);
-        //searchInput.setOnKeyListener(SearchFieldListener);
+        actv = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView1);
+        /*
         searchInput.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String s) {
@@ -95,6 +113,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 return false;
             }
         });
+        */
     }
 
     /**
