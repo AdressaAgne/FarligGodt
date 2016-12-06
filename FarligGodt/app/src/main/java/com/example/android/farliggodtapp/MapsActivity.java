aa5b6d7cb3741c0ca38eca0ac569c0cebba74356
miatollaksvik <design@miatollaksvik.no>
2016-12-06 13:59:39 +0100

    deprecated code

--- FarligGodt/app/src/main/java/com/example/android/farliggodtapp/MapsActivity.java
+++ FarligGodt/app/src/main/java/com/example/android/farliggodtapp/MapsActivity.java
@@ -5,12 +5,10 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.database.MatrixCursor;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.provider.BaseColumns;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
@@ -19,7 +17,6 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.SearchView;
-import android.widget.SimpleCursorAdapter;
 import android.widget.Toast;
 
 import com.example.android.farliggodtapp.api.Api;
@@ -55,13 +52,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     private boolean customLocation = false;
 
-    private SimpleCursorAdapter mAdapter;
     private SearchView searchInput;
-    String[] SUGGESTIONS = {
-            "Thomas", "Mia", "Agne",
-            "Øyvind", "skjiitsekk", "Snerkefjas",
-            "Heisann", "Trolololol"
-    };
 
 
 
@@ -101,20 +92,9 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             @Override
             public boolean onQueryTextChange(String s) {
                 Log.i("TEXT CHANGED", s);
-
-
                 return false;
             }
         });
-    }
-
-    private void populateAdapter(String query) {
-        final MatrixCursor c = new MatrixCursor(new String[]{ BaseColumns._ID, "cityName" });
-        for (int i=0; i<SUGGESTIONS.length; i++) {
-            if (SUGGESTIONS[i].toLowerCase().startsWith(query.toLowerCase()))
-                c.addRow(new Object[] {i, SUGGESTIONS[i]});
-        }
-        mAdapter.changeCursor(c);
     }
 
     /**
